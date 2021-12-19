1f2081bcf11e7663a8669f131ef88cacf1a7dd23
nur <medo1208@yahoo.com>
2021-12-19 16:36:48 +0800

    fix the error

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const axios = require("axios");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3030;
 
+const call = async (num) => {
+  await axios.get(`http://localhost:3030/?number=${num}`);
+};
 app.get("/", (req, res) => {
+  let num = 0;
+  setTimeout(() => {
+    call(Math.random());
+  }, 1000 * 1);
+  num = num + 1;
+  console.log(req.query.number);
   res.send(`Hello World! ${req.query.number}`);
 });
 
 app.listen(port, () => {
   console.log(`express app listening at http://localhost:${port}`);
 });
-
-export const call = async (num) => {
-  await axios.get(
-    `https://test-vercel-six-psi.vercel.app?number=${num}`
-  );
-};
-
-while (true) {
-  let num = 0;
-  setTimeout(() => {
-    call(Math.random());
-  }, 1000 * 10);
-  num = num + 1;
-}
