427c27a825a29b638f99120925de8daa652829d3
nur <medo1208@yahoo.com>
2021-12-19 16:22:46 +0800

    add call the api function

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+const axios = require("axios");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`Hello World! ${req.query.number}`);
 });
 
 app.listen(port, () => {
   console.log(`express app listening at http://localhost:${port}`);
 });
+
+export const call = async (num) => {
+  await axios.get(
+    `https://test-vercel-six-psi.vercel.app?number=${num}`
+  );
+};
+
+while (true) {
+  let num = 0;
+  setTimeout(() => {
+    call(Math.random());
+  }, 1000 * 10);
+  num = num + 1;
+}
